8028f1b7477643f5608144b710355d3aa44c48c7
Vyvy-vi <vyom.j@protonmail.com>
2021-03-28 05:48:11 +0530

    feat: added hCaptcha

--- app/app.py
+++ app/app.py
@@ -3,19 +3,23 @@ import requests
 
 from dotenv import load_dotenv
 from flask import Flask, redirect, url_for, render_template
-from flask import send_from_directory
+from flask import send_from_directory, request
 from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
 
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.environ["SECRET_KEY"]
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "false"      # !! Only in development environment.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "false"      # !! True Only in development environment.
 
 app.config["DISCORD_CLIENT_ID"] = os.environ["DISCORD_CLIENT_ID"]  # Discord client ID.
 app.config["DISCORD_CLIENT_SECRET"] = os.environ["DISCORD_CLIENT_SECRET"]              # Discord client secret.
 app.config["DISCORD_REDIRECT_URI"] = f"{os.environ['HOST_DOMAIN']}/callback"
 app.config["DISCORD_BOT_TOKEN"] = os.environ["DISCORD_BOT_TOKEN"]
 app.config["DISCORD_GUILD_ID"] = os.environ["DISCORD_GUILD_ID"]
+CAPTCHA_KEY = os.environ["CAPTCHA_KEY"]
+SITE_KEY = os.environ["SITE_KEY"]
+
+VERIFY_URL = "https://hcaptcha.com/siteverify"
 
 discord = DiscordOAuth2Session(app)
 
@@ -41,25 +45,28 @@ def callback():
 def redirect_unauthorized(e):
     return redirect(url_for("login"))
 
-@app.route("/me/")
+@app.route("/me/", methods=['GET', 'POST'])
 @requires_authorization
 def me():
-    user = discord.fetch_user()
-    return render_template('join.html', user=user.name, img=user.avatar_url, _link='/join')
-
-@app.route("/join/")
-@requires_authorization
-def join():
-    _id = discord.fetch_user().id
-    data = {'access_token': discord.get_authorization_token()['access_token']}
-    url = f"https://discord.com/api/guilds/{app.config['DISCORD_GUILD_ID']}/members/{_id}"
-    headers = {
-            "Authorization" : f"Bot {app.config['DISCORD_BOT_TOKEN']}",
-            'Content-Type': 'application/json'
-            }
-    res =requests.put(url=url, json=data, headers=headers)
-    print(res.status_code)
-    return "You have been added to the server"
+    if request.method == 'GET':
+        user = discord.fetch_user()
+        return render_template('join.html', user=user.name, img=user.avatar_url, key=SITE_KEY)
+    elif request.method == 'POST':
+        token = request.form['h-captcha-response']
+        payload = { 'secret': CAPTCHA_KEY, 'response': token }
+        res = requests.post(url=VERIFY_URL, data=payload)
+        if res.status_code == 200:
+            if res.json()['success'] == True:
+                _id = discord.fetch_user().id
+                data = {'access_token': discord.get_authorization_token()['access_token']}
+                url = f"https://discord.com/api/guilds/{app.config['DISCORD_GUILD_ID']}/members/{_id}"
+                headers = {
+                    "Authorization": f"Bot {app.config['DISCORD_BOT_TOKEN']}",
+                    "Content-type": "application/json"
+                }
+                res = requests.put(url=url, json=data, headers=headers)
+                return "You have been added to the server"
+        return redirect(url_for('me'))
 
 if __name__ == "__main__":
     app.run()
